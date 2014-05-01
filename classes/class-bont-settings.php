fb5396414a4978c6021ca8f68beb7e4095d4a1c1
KingYes <KingYes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-01 20:11:31 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/bontact/tags/1.0.2@906506 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class-bont-settings.php
@@ -0,0 +1,193 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class BONT_Settings {
+
+	/**
+	 * @var array
+	 */
+	protected $_form_messages = array();
+	
+	/**
+	 * @var string
+	 */
+	protected $_option_slug;
+	
+	protected $_api_login_url = 'https://dashboard.bontact.com/api/bontactapi.aspx?func=login&username=%s&password=%s';
+	
+	protected function _do_redirect_option_page( $message_code = null ) {
+		$return_url = add_query_arg( 'page', $this->_option_slug, admin_url( 'admin.php' ) );
+		
+		if ( ! is_null( $message_code ) )
+			$return_url = add_query_arg( 'message', $message_code, $return_url );
+		
+		wp_redirect( $return_url );
+		die();
+	}
+	
+	protected function _get_logout_link() {
+		$url = add_query_arg( array( 'page' => $this->_option_slug, 'bont-action' => 'logout' ), admin_url( 'options-general.php' ) );
+		return wp_nonce_url( $url, 'bontact_logout_' . get_current_user_id() );
+	}
+	
+	public function admin_init() {
+		if ( ! current_user_can( 'manage_options' ) )
+			return;
+
+		if ( ! empty( $_REQUEST['bont-action'] ) ) {
+			if ( 'logout' === $_REQUEST['bont-action'] ) {
+				check_admin_referer( 'bontact_logout_' . get_current_user_id() );
+				delete_option( $this->_option_slug );
+				$this->_do_redirect_option_page( 5 );
+			}
+
+			if ( 'login' === $_REQUEST['bont-action'] ) {
+				if ( empty( $_REQUEST['nonce'] ) || ! check_ajax_referer( 'bontact_' . $_REQUEST['bont-action'] . '_' . get_current_user_id(), 'nonce', false ) ) {
+					$this->_do_redirect_option_page( 6 );
+				}
+
+				$response = wp_remote_get( sprintf( $this->_api_login_url, $_POST['bontact']['username'], $_POST['bontact']['password'] ), array( 'sslverify' => false, 'timeout' => 30 ) );
+
+				if ( is_wp_error( $response ) || 200 !== $response['response']['code'] ) {
+					$this->_do_redirect_option_page( 4 );
+				}
+
+				$data_return = json_decode( $response['body'] );
+				if ( is_null( $data_return ) || '200' !== $data_return->code ) {
+					$this->_do_redirect_option_page( 3 );
+				}
+
+				$options = get_option( $this->_option_slug, array() );
+				$options['token'] = $data_return->token;
+				$options['username'] = $_POST['bontact']['username'];
+				$options['password'] = $_POST['bontact']['password'];
+
+				update_option( $this->_option_slug, $options );
+				
+				$this->_do_redirect_option_page( 2 );
+			}
+		}
+	}
+	
+	public function bontact_setting_content() {
+		$username = $this->get_option( 'username' );
+		$password = $this->get_option( 'password' );
+		?>
+		<!-- Create a header in the default WordPress 'wrap' container -->
+		<div class="wrap bontact-admin-wrap">
+			<div id="icon-themes" class="icon32"></div>
+			<h2><?php _e( 'Bontact Settings', 'bontact' ); ?></h2>
+			
+			<?php if ( ! empty( $_GET['message'] ) && ! empty( $this->_form_messages[ $_GET['message'] ] ) ) : ?>
+			<div class="<?php echo $this->_form_messages[ $_GET['message'] ]['status']; ?>"><p><?php echo $this->_form_messages[ $_GET['message'] ]['msg']; ?></p></div>
+			<?php endif; ?>
+			<?php if ( ! empty( $username ) && ! empty( $password ) ) : ?>
+				<h3><?php _e( 'Bontact Widget:', 'bontact' ); ?></h3>
+				<p><?php _e( 'Bontact adds a widget to your website which enables customers to contact you anytime, in any way – live chat, click-to-call, text messages or email.', 'bontact' ); ?></p>
+				
+				<p><?php _e( 'You are logged in as', 'bontact' ); ?> <strong><?php echo $username; ?></strong></p>
+				
+				<p class="bont-get-space">
+					<a class="button-primary" href="http://dashboard.bontact.com/html/chatDashboard.aspx" target="_blank"><?php _e( 'Click here to access the full Bontact dashboard', 'bontact' ); ?></a><br /><br />
+					<?php _e( 'The Bontact Dashboard offers full functionality and also allows you to provide new reps with their own password.', 'bontact' ); ?>
+				</p>
+
+				<p><?php _e( sprintf( 'Need help? Contact us at <a href="mailto:%1$s">%1$s</a> and we\'ll provide you with any info you need.', 'sales@bontact.com' ), 'bontact' ); ?></p>
+
+				<hr />
+				
+				<h3><?php _e( 'Follow us:', 'bontact' ); ?></h3>
+				<div>
+					<ul class="bont-ul-circle">
+						<li><a href="https://www.facebook.com/bontact" target="_blank"><?php _e( 'Facebook', 'bontact' ); ?></a></li>
+						<li><a href="https://plus.google.com/u/0/106943736208504305957/posts" target="_blank"><?php _e( 'Google+', 'bontact' ); ?></a></li>
+						<li><a href="https://twitter.com/bontact" target="_blank"><?php _e( 'Twitter', 'bontact' ); ?></a></li>
+						<li><a href="http://www.youtube.com/user/Bontact" target="_blank"><?php _e( 'YouTube', 'bontact' ); ?></a></li>
+					</ul>
+				</div>
+
+				<hr class="bont-get-space" />
+				
+				<p><a class="button" href="<?php echo $this->_get_logout_link(); ?>" onclick="return confirm('<?php _e( 'Are you sure you want to disconnect your Bontact account from your WordPress site?', 'bontact' ); ?>');"><?php _e( 'Disconnect your Bontact account from your WordPress site', 'bontact' ); ?></a></p>
+			<?php else : ?>
+				
+				<h3><?php _e( 'Login using your Bontact account:', 'bontact' ); ?></h3>
+				<p class="description">
+					<?php printf( __( '<a href="%s" target="_blank">click here</a> to sign up for a free account', 'bontact' ), 'http://bontact.com/free-account-for-wordpress-users/?wordpress-bontact&utm_source=Plugins&utm_medium=WP&utm_campaign=WP-Bontact-Plugin' ); ?>
+				</p>
+				<form action="" method="post">
+					<input type="hidden" name="page" value="<?php echo $this->_option_slug; ?>" />
+					<input type="hidden" name="nonce" value="<?php echo wp_create_nonce( 'bontact_login_' . get_current_user_id() ); ?>" />
+					<input type="hidden" name="bont-action" value="login" />
+					<table class="form-table">
+						<tr valign="top">
+							<th scope="row">
+								<label for="bontact_username"><?php _e( 'Username (email):', 'bontact' ); ?></label>
+							</th>
+							<td>
+								<input id="bontact_username" type="text" name="bontact[username]" value="" autocomplete="off" />
+							</td>
+						</tr>
+						<tr valign="top">
+							<th scope="row">
+								<label for="bontact_password"><?php _e( 'Password:', 'bontact' ); ?></label>
+							</th>
+							<td>
+								<input id="bontact_password" type="password" name="bontact[password]" value="" autocomplete="off" />
+							</td>
+						</tr>
+					</table>
+					<div class="submit">
+						<input type="submit" name="type-submit" class="button button-primary" value="<?php _e( 'Login', 'bontact' ); ?>" />
+					</div>
+				</form>
+			<?php endif; ?>
+		</div><!-- /.wrap -->
+	<?php
+	}
+	
+	public function admin_menu() {
+		add_menu_page( 'Bontact Settings', 'Bontact', 'manage_options', 'bontact-settings', array( &$this, 'bontact_setting_content' ), plugins_url( 'assets/images/logo-bontact-16x16.png', BONTACT_BASE ) );
+	}
+	
+	public function get_option( $key ) {
+		$options = get_option( $this->_option_slug );
+		return isset( $options[ $key ] ) ? $options[ $key ] : '';
+	}
+	
+	public function __construct() {
+		$this->_form_messages = array(
+			'', // Just skip from zero array.
+			array(
+				'msg' => __( 'Registration was successful.', 'bontact' ),
+				'status' => 'updated',
+			),
+			array(
+				'msg' => __( 'Login was successful.', 'bontact' ),
+				'status' => 'updated',
+			),
+			array(
+				'msg' => __( 'Invalid login.', 'bontact' ),
+				'status' => 'error',
+			),
+			array(
+				'msg' => __( 'Error with API server.', 'bontact' ),
+				'status' => 'error',
+			),
+			array(
+				'msg' => __( 'Your logout was successful.', 'bontact' ),
+				'status' => 'updated',
+			),
+			array(
+				'msg' => __( 'Action expired.', 'bontact' ),
+				'status' => 'error',
+			),
+		);
+		$this->_option_slug = 'bontact-settings';
+		
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+	}
+	
+}
---
+++ readme.txt
@@ -0,0 +1,88 @@
+=== Bontact ===
+Contributors: KingYes, bontact
+Tags: callback, live chat, call back, text messages, sms, customer engagement platform, conversion, increase conversion
+Requires at least: 3.5
+Tested up to: 3.9
+Stable tag: 1.0.2
+License: GPLv2 or later
+
+== Short Description ==
+Bontact helps you connect with your website visitors and turn them into converting customers, through a multi-channel customer engagement platform, Including  text messages, callback, e-mail, and live chat options.
+
+
+== Description ==
+Bontact helps you connect with your website visitors and turn them into converting customers, through a multi-channel, flexible, real-time, and easy-to-use customer engagement platform. The platform is the only solution of its kind to combine text messages, callback, e-mail, and live chat options.
+
+Each of your customers may have a different preferred method of contact.  Presenting potential customers, while they are at the peak of interest, with flexible options to connect with your sales team has tremendous business potential. It enhances user experience and satisfaction, increases the “stickiness” of your website (average visit duration), and – most importantly — can significantly increase the number of leads and sales. Combined with the right sales and support staff, Bontact can even turn random online visitors into committed customers.
+
+
+http://www.youtube.com/watch?v=FFsJ31Q2dI8
+
+
+* Phone – The “Callback” feature enables users to enter their phone number (landline or mobile anywhere in the world). They are immediately called back. Within seconds they are connected with with you sales or support representatives, at no cost or hassle for the customer.
+* Chat – The “Chat” feature enables users to click a text box and begin chatting, in real time, with your company’s live, online representative. The chat can also be initiated by the representative.
+* Text Message- The “SMS” feature enables users to send a free text message from the website to your representative’s cell phone. Your representative can respond by SMS immediately.
+* E-Mail – The “E-Mail” feature enables users to fill a quick contact form which is sent directly to your representatives’’ in-box and allows them respond by e-mail at a later time.
+
+Solid ROI
+Bontact Live Support Widget turns your website visitors into paying customers. What is the value of a single customer to you? Bontact packages are so affordable that they need to generate only a few sales to pay for themselves for a full year of use.
+
+Effortless Setup
+After installing the Bontact widget, the Bontact Live Support Widget immediately appear on any page of your choice on your website Now, Web visitors will have the choice of clicking on their preferred mode of communication (phone, text message, live chat or E-Mail) to reach out to your sales or customer support representatives.
+
+== Installation ==
+Install plugin from WordPress directory and activate it.
+
+If you are already registered to Bontact enter your user name and password. If not, define a username and password. 
+
+The widget puts the installation code in the website's pages all of them. Click the blue button "click here to access the full bontact dashboard". here you will define the settings. select the services you want. features, company details. phone number, reps 
+
+Finally, make full use of our intuitive Dashboard to manage your chat widget.
+
+= Requirements =
+* __Requires PHP5__ for list management functionality.
+
+= What is the plugin license? =
+
+* This plugin is released under a GPL license.
+
+== Frequently Asked Questions ==
+
+= What is Callback? =
+Bontact allows users to call your sales or customer support representatives by clicking a button, without picking up a phone.
+
+= Can I see a list of all the calls that been made? =
+Yes. Once you sign up, you receive a user name and password for the dashboard. There you may view the time of the call and call duration.
+
+= Why should I use Bontact’s Callback service and not just publish a phone number? =
+You can do both. But Callback cannot be compared to a regular phone number. The Callback feature on your website is a value-add service that helps you generate more inquiries. It gives you an edge over your competitors and adds a “human touch” to your Website. As opposed to regular phone calls, Callback is also completely effortless and free for customers.
+
+= How does the Callback service work? =
+Every Callback results in two calls – one to the web visitor and another one to you. We use normal PSTN Lines (NOT VoIP) to maintain a high quality of voice call and fast connection.
+
+= Isn’t toll free a cheaper option? =
+Toll free is actually a more costly solution. It also requires you to set up proper hardware and is not ideal for smaller companies.
+Additional features like call recordings, SMS alerts, web- based reporting systems, etc. could only be made available at additional cost. We offer these services within the packages.
+
+= How about my international users? =
+All of our services are offered locally and globally. We support most languages (Chat) and offer calls to any destination in the world (Callback/SMS).
+
+= How long does the callback process take? =
+Within a few seconds the customer will be called back, and connected with your representative.
+
+= What happens if all my incoming lines are busy? =
+Customers will hear an announcement explaining that you are unavailable to take their call at the time. Their details will be logged and you may choose to call them back at a later time.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.2 =
+* Fixed some bugs
+
+= 1.0.1 =
+* Tested up to WordPress 3.9
+
+= 1.0 =
+* Blastoff!
